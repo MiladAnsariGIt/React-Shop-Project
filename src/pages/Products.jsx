2d56ce70f834788fea15022373c729c1e53ee07f
HP <acforapex1382@gmail.com>
2026-08-17 15:48:21 +0330

    improved products searching with adding price and rating options
    Added:
    - sorting products by price and rating
    - pagination

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,6 +8,9 @@ function Products(){
     const {data,loading,error} = useFetch("http://localhost:3000/products");
     const [search,setSearch] = useState("");
     const [category,setCategory] = useState("all");
+    const [sort,setSort] = useState("default");
+    const [currentPage,setCurrentPage] = useState(1);
+    const productsPerPage = 6;
 
     if(loading){
         return <h2>Loading...</h2>
@@ -28,27 +31,61 @@ function Products(){
     return matchesSearch && matchesCategory;
     });
 
+    const sortedData = [...filteredData].sort((a,b) => {
+        if(sort === "price-low")
+            return a.price - b.price;
+        if(sort === "price-high")
+            return b.price - a.price;
+        if(sort === "rating-high")
+            return b.rating - a.rating
+        return 0;
+    })
+
+    // pagination
+    const startIndex = (currentPage - 1) * productsPerPage;
+    const endIndex = startIndex + productsPerPage;
+    const currentProducts = sortedData.slice(startIndex,endIndex);
+
+    const totalPages = Math.ceil(sortedData.length / productsPerPage);
+
     return(
         <div>
             <input type="text"
              value={search} 
-             onChange={e => setSearch(e.target.value)}
+             onChange={e => {setSearch(e.target.value);
+                setCurrentPage(1);
+             }}
              placeholder="Search product..."
              />
-             <select value={category} onChange={e => setCategory(e.target.value)}>
+             <select value={category} onChange={e => { setCategory(e.target.value);
+                setCurrentPage(1);
+             }}>
                 <option value="all">All</option>
                 <option value="keyboard">Keyboard</option>
                 <option value="mouse">Mouse</option>
                 <option value="headphone">Headphone</option>
              </select>
+             <select value={sort} onChange={e => { setSort(e.target.value);
+                setCurrentPage(1);
+             }}>
+                <option value="default">Default</option>
+                <option value="price-low">Price: Low → High</option>
+                <option value="price-high">Price: High → Low</option>
+                <option value="rating-high">Rating: High → Low</option>
+             </select>
             <br />
             <h2>Products</h2>
-           { filteredData.map(product => (
+           {currentProducts.length === 0 ? <h3>no products found!</h3> : currentProducts.map(product => (
                 <ProductCard
                     key={product.id}
                     product={product}
                 />
             ))}
+            <div>
+            {Array.from({length: totalPages}).map((_,index) => (
+                <button key={index} onClick={() => setCurrentPage(index+1)}>{index+1}</button>
+            ))}
+           </div>
         </div>
     )
 
